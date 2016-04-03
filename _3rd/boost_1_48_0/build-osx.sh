d9d7124ea6415d77365dda0c06d0d23ade88aa32
d0nn13 <samir.ahamada89@gmail.com>
2016-04-03 10:33:20 +0200

    Patch OSX 10.11 build with GCC
    GCC 5.3.0 from Homebrew

--- _3rd/boost_1_48_0/build-osx.sh
+++ _3rd/boost_1_48_0/build-osx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./b2 link=static architecture=x86 address-model=32_64 macosx-version=10.7 macosx-version-min=10.6 --stagedir=./stage --with-iostreams --with-program_options stage 
+./b2 link=static architecture=x86 address-model=64 --stagedir=./stage --with-iostreams --with-program_options stage
